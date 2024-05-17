259307fdbfea16471be63315cf85cd8d82d85217
sam-medley <Blazer2sam@gmail.com>
2024-05-17 04:44:26 -0400

    created todo.html which hosts the todo list. The add and remove features are implemented. Created folder for md files

---
+++ todo.js
@@ -0,0 +1,29 @@
+document.getElementById('taskForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  const taskInput = document.getElementById('newTask'); //creating a object that is the users inputed task
+  //if the input is empty it will not add a task
+  if (taskInput.value.trim() !== ''){
+    addTask(taskInput.value.trim()); //Add the task
+    taskInput.value = ''; //Clear the input field
+  } 
+});
+
+function addTask(task){
+    const taskList = document.getElementById('taskList');
+    const listItem = document.createElement('listItem'); 
+    listItem.className = 'list-group-item';
+    listItem.innerText = task; //the text property of the object = the string task which gets passed as a parameter 
+    listItem.addEventListener('click', function(){ //when the add button is clicked, the item text color becomes green
+        this.classList.toggle('list-group-item-success');
+    });
+
+    const removeBtn = document.createElement('button'); //creating remove object for when we want to remove tasks.
+    removeBtn.className = 'btn btn-danger btn-sm float-end'; //a remove button is created and implemented into the task itself
+    removeBtn.innerText = 'Remove';
+    removeBtn.addEventListener('click', function() {
+        taskList.removeChild(listItem);
+    });
+
+    listItem.appendChild(removeBtn);
+    taskList.appendChild(listItem);
+}
