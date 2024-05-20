4429ac638d2b030f17d80807e336d736ab4bcf69
sam-medley <Blazer2sam@gmail.com>
2024-05-20 04:45:53 -0400

    edit toggles visibility of remove buttons

--- tasks/todo.md
+++ tasks/todo.md
@@ -5,5 +5,6 @@ The user will be able to add/remove tasks. The users character will also be disp
 **what needs to be added**
 - create a bank object for storing user currency
 - have an integer in the bank object increase when a task is removed. This will be used as the users currency
-- add a delete/edit feature for each task
+- have edit toggle remove buttons off also
+- have the delete buttons remove the task without incrementing bank object
 - draft a css design for the page
--- todo.js
+++ todo.js
@@ -6,6 +6,7 @@ document.getElementById('taskForm').addEventListener('submit', function(event) {
     addTask(taskInput.value.trim()); //Add the task
     taskInput.value = ''; //Clear the input field
   } 
+
 });
 
 function addTask(task){
@@ -13,18 +14,39 @@ function addTask(task){
 
     //creating a <div> for each button with the "col-12 mb-2 classname. This way the buttons stack ontop of each other"
     var newDiv = document.createElement("div");
-    newDiv.className = "col-12 mb-2";
+    newDiv.className = "col-md-6";
   
     const listItem = document.createElement('button'); //creates a button which will be used as a listItem that displays the task
     listItem.className = 'btn btn-secondary w-100';
     listItem.innerText = task; //the text property of the object = the string task which gets passed as a parameter in this function
+
+    //these are the edit buttons that appear next to each task. They will appear as invisible and locked until the user clicks the edit button.
+    var editDiv = document.createElement("div");
+    editDiv.className = "col-md-6";
+    editDiv.style.visibility = 'hidden';
+    const editItem = document.createElement('button');
+    editItem.className = 'btn btn-sencondary w-100';
+    editItem.innerText = 'delete';
     
     //when a task is clicked, the newDiv is removed, which deletes the task from the list 
     listItem.addEventListener('click', function(){ 
         taskList.removeChild(newDiv);
+        taskList.removeChild(editDiv);
     });
 
     //the newDiv appends the listItem and the taskList appends the newDiv
     newDiv.append(listItem);
     taskList.appendChild(newDiv);
+    editDiv.append(editItem);
+    taskList.appendChild(editDiv);
+
+}
+
+//makes the editDiv which holds the delete buttons visible
+function toggleEdit(){
+  const taskList = document.getElementById('taskList');
+  for(let i=1; i<=taskList.childElementCount; i++){
+    console.log(i);
+    taskList.children[i].style.visibility = 'visible';
+  }
 }
