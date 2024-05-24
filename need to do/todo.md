93267f50bb79b11cee2b5bc5f67be7f50e4b7b93
sam-medley <Blazer2sam@gmail.com>
2024-05-24 03:19:42 -0400

    input locks when in edit mode

--- need to do/todo.md
+++ need to do/todo.md
@@ -7,5 +7,4 @@ The user will be able to add/remove tasks. The users character will also be disp
 - have an integer in the bank object increase when a task is removed. This will be used as the users currency
 - have the delete buttons remove the task without incrementing bank object
 - add css trash bin animation for delete buttons
-- fix bug where delete buttons stay on when a new task is added. This can be done by locking the task input until the user clicks edit again. 
 - draft a css design for the page
--- todo.js
+++ todo.js
@@ -1,63 +1,64 @@
 document.getElementById('taskForm').addEventListener('submit', function(event) {
-  event.preventDefault(); 
-  const taskInput = document.getElementById('newTask'); //creating a object that is the users inputed task
+  event.preventDefault();
+  const taskInput = document.getElementById('newTask'); //creating an object that is the users inputed task
   //if the input is empty it will not add a task
   if (taskInput.value.trim() !== ''){
     addTask(taskInput.value.trim()); //Add the task
     taskInput.value = ''; //Clear the input field
-  } 
-
+  }
 });
 
 function addTask(task){
-    const taskList = document.getElementById('taskList');
+  const taskList = document.getElementById('taskList');
 
-    //creating a <div> for each button with the "col-12 mb-2 classname. This way the buttons stack ontop of each other"
-    var newDiv = document.createElement("div");
-    newDiv.className = "col-md-6";
-  
-    const listItem = document.createElement('button'); //creates a button which will be used as a listItem that displays the task
-    listItem.className = 'btn btn-secondary w-100';
-    listItem.innerText = task; //the text property of the object = the string task which gets passed as a parameter in this function
-    listItem.id = 'strikeButton';
+  //creating a <div> for each button with the "col-12 mb-2 classname. This way the buttons stack on top of each other"
+  var newDiv = document.createElement("div");
+  newDiv.className = "col-md-6";
 
-    //these are the edit buttons that appear next to each task. They will appear as invisible and locked until the user clicks the edit button.
-    var editDiv = document.createElement("div");
-    editDiv.className = "col-md-6";
-    editDiv.style.visibility = 'hidden';
-    const editItem = document.createElement('button');
-    editItem.className = 'btn btn-sencondary w-100';
-    editItem.innerText = 'delete';
-    
-    //when a task is clicked, the newDiv is removed, which deletes the task from the list 
-    listItem.addEventListener('click', function(){ 
-        taskList.removeChild(newDiv);
-        taskList.removeChild(editDiv);
-    });
+  const listItem = document.createElement('button'); //creates a button which will be used as a listItem that displays the task
+  listItem.className = 'btn btn-secondary w-100';
+  listItem.innerText = task; //the text property of the object = the string task which gets passed as a parameter in this function
+  listItem.id = 'strikeButton';
 
-    editItem.addEventListener('click', function(){
-      taskList.removeChild(newDiv);
-      taskList.removeChild(editDiv);
-    });
+  //these are the edit buttons that appear next to each task. They will appear as invisible and locked until the user clicks the edit button.
+  var editDiv = document.createElement("div");
+  editDiv.className = "col-md-6";
+  editDiv.style.visibility = 'hidden';
+  const editItem = document.createElement('button');
+  editItem.className = 'btn btn-secondary w-100';
+  editItem.innerText = 'delete';
 
-    //the newDiv appends the listItem and the taskList appends the newDiv
-    newDiv.append(listItem);
-    taskList.appendChild(newDiv);
-    editDiv.append(editItem);
-    taskList.appendChild(editDiv);
+  //when a task is clicked, the newDiv is removed, which deletes the task from the list 
+  listItem.addEventListener('click', function(){
+    taskList.removeChild(newDiv);
+    taskList.removeChild(editDiv);
+  });
+
+  editItem.addEventListener('click', function(){
+    taskList.removeChild(newDiv);
+    taskList.removeChild(editDiv);
+  });
+
+  //the newDiv appends the listItem and the taskList appends the newDiv
+  newDiv.append(listItem);
+  taskList.appendChild(newDiv);
+  editDiv.append(editItem);
+  taskList.appendChild(editDiv);
 }
 
 //makes the editDiv which holds the delete buttons visible
 function toggleEdit(){
   const taskList = document.getElementById('taskList');
-  for(let i=1; i<=taskList.childElementCount; i++){
+  const taskInput = document.getElementById('newTask'); // Ensure taskInput is referenced correctly
 
+  for(let i=0; i<taskList.childElementCount; i++){
     //checks to see if the button is visible and even(an even button will always be a delete button)
-    if(taskList.children[i].style.visibility == 'visible' && i%2 != 0){
+    if(taskList.children[i].style.visibility == 'visible' && i % 2 != 0){
       taskList.children[i].style.visibility = 'hidden';
-    }
-    else{
+      taskInput.disabled = false;
+    } else {
       taskList.children[i].style.visibility = 'visible'; //makes button visible if it is already hidden
+      taskInput.disabled = true; // Disable the task input field
     }
   }
 }
